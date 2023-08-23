3ebd27e4d52f064d31ea424a77ce070b9c482f01
Albert Waz <albewaz@gmail.com>
2023-08-23 17:54:52 +0200

    adding focus

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { Form, Input, Button } from "./styled";
 
 const TaskForm = ({ addNewTask }) => {
@@ -7,23 +7,29 @@ const TaskForm = ({ addNewTask }) => {
   const onFormSubmit = (event) => {
     event.preventDefault();
     if (newTaskContent.trim() === "") {
-      return;  
+      return;
     }
 
     addNewTask(newTaskContent.trim());
-    setNewTaskContent(""); 
+    setNewTaskContent("");
+  };
+  const inputRef = useRef(null);
+
+  const focusInput = () => {
+    inputRef.current.focus();
   };
 
   return (
     <Form onSubmit={onFormSubmit}>
       <Input
+        ref={inputRef}
         value={newTaskContent}
         autoFocus
         name="task"
         placeholder="Co jest do zrobienia?"
         onChange={({ target }) => setNewTaskContent(target.value)}
       />
-      <Button>Dodaj zadanie</Button>
+      <Button onClick={focusInput}>Dodaj zadanie</Button>
     </Form>
   );
 };
